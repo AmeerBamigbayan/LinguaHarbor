8a767f68728867b72157e769292d91b903e43342
Ameer_105 <ameerdamilola@gmail.com>
2024-04-01 00:52:38 +0100

    increased "WHY CHOOSE US" section width

--- script.js
+++ script.js
@@ -819,6 +819,9 @@ tailwind.config = {
             64: '16rem',
             72: '18rem',
             80: '20rem',
+            84: '21rem',
+            88: '22rem',
+            92: '23rem',
             96: '24rem',
           },
           stroke: ({ theme }) => ({
