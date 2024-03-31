0974afe2c98d49423514dfdca9acd6ad00d23bba
Ameer_105 <ameerdamilola@gmail.com>
2024-03-31 19:24:36 +0100

    added russian course card

--- script.js
+++ script.js
@@ -109,6 +109,8 @@ tailwind.config = {
             xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
             '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
             inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
+            cards: '0px 4px 20px 1px rgba(0, 0, 0, 0.17)',
+
             none: 'none',
           },
           boxShadowColor: ({ theme }) => theme('colors'),
