279df71c3f14451d62e9d82a386d7a17c9a11c9f
Ameer_105 <ameerdamilola@gmail.com>
2024-04-06 03:43:06 +0100

    done with hero section mobile responsiveness

--- script.js
+++ script.js
@@ -345,6 +345,7 @@ tailwind.config = {
             ],
           },
           fontSize: {
+            xxs: ['10px', { lineHeight: '1rem' }],
             xs: ['0.75rem', { lineHeight: '1rem' }],
             sm: ['0.875rem', { lineHeight: '1.25rem' }],
             base: ['1rem', { lineHeight: '1.5rem' }],
